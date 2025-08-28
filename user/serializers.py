704498c25170511e59b185b2936730294b25791d
migyeong <wjdalrud5516@gmail.com>
2025-08-28 11:21:48 +0900

    시리얼라이저 업데이트

--- user/serializers.py
+++ user/serializers.py
@@ -9,3 +9,19 @@ class UserSerializer(serializers.ModelSerializer):
             'date_joined', 'cognito_sub', 'cognito_status', 'is_active', 'last_login'
         ]
         read_only_fields = ['idx', 'date_joined', 'last_login', 'cognito_sub', 'cognito_status']
+class SignupSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    password = serializers.CharField(max_length=128, write_only=True)
+    email = serializers.CharField(max_length=254)
+    tel = serializers.CharField(max_length=20, required=False, allow_blank=True)
+    birth = serializers.CharField(max_length=10, required=False, allow_blank=True)
+    team = serializers.CharField(max_length=20, required=False, allow_blank=True)
+    name = serializers.CharField(max_length=150, required=False, allow_blank=True)
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    password = serializers.CharField(max_length=128)
+
+class ConfirmSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    code = serializers.CharField(max_length=6)
