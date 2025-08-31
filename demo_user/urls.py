8c478d60d5448c4ea97c538e2e4a89c9ae564195
migyoeng <wjdalrud5516@gmail.com>
2025-08-31 20:55:17 +0900

    health check 경로 추가

--- demo_user/urls.py
+++ demo_user/urls.py
@@ -11,14 +11,20 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. Import the include() function: from my_app.views import Home
+    2. Add a URL to urlpatterns: path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def healthz(_):
+    """ALB 헬스체크용 엔드포인트"""
+    return HttpResponse("ok")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('user.urls')),
     path('api/', include('user.api_urls')),
+    path('healthz', healthz, name='healthz'),  # ALB 헬스체크용
 ]
