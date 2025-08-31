95eba20513ac6b7cb88f5e1164d260b7763e70a1
migyoeng <wjdalrud5516@gmail.com>
2025-08-31 20:26:58 +0900

    CORS 설정 변경

--- demo_user/settings.py
+++ demo_user/settings.py
@@ -147,10 +147,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS 설정
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://localhost:5173",
-    "http://127.0.0.1:3000",
-    "http://127.0.0.1:5173",
+    "*"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
