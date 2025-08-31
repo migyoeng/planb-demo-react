420a2b28ccdd47baf774008d281e4cd3008f40fb
migyoeng <wjdalrud5516@gmail.com>
2025-08-31 21:12:56 +0900

    CORS 오리진 전부 허용..

--- demo_user/settings.py
+++ demo_user/settings.py
@@ -148,9 +148,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS 설정
-CORS_ALLOWED_ORIGINS = [
-    "*"
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
 
